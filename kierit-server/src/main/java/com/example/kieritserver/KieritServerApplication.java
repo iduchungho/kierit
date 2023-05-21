8a28b9a50b61518b04c2d1214a2590c0f92c84d8
Kawasaki <iduchungho@gmail.com>
2023-05-21 20:18:27 +0700

    initial commit

---
+++ kierit-server/src/main/java/com/example/kieritserver/KieritServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kieritserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieritServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieritServerApplication.class, args);
+	}
+
+}
---
+++ kierit-server/src/test/java/com/example/kieritserver/KieritServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kieritserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KieritServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
